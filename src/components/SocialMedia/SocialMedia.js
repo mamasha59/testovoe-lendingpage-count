d0fead806c5866207ee11ed0ac2f38f80439be06
Alesha Tikhonov <zukko961@gmail.com>
2023-04-28 16:37:49 +0300

    w h

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -6,9 +6,9 @@ export default function SocialMedia() {
 
   return (
     <>
-        <li className='cursor-pointer hover:opacity-60 w-5 h-5'><img src={telegram} alt="соц. сеть telegram" title='соц. сеть telegram' aria-label='соц. сеть telegram'/></li>
-        <li className='cursor-pointer hover:opacity-60 w-5 h-5'><img src={viber} alt="соц. сеть viber" title='соц. сеть viber' aria-label='соц. сеть viber'/></li>
-        <li className='cursor-pointer hover:opacity-60 w-5 h-5'><img src={whatsapp} alt="соц. сеть whatsapp" title='соц. сеть whatsapp' aria-label='соц. сеть whatsapp'/></li>
+        <li className='cursor-pointer hover:opacity-60 w-5'><img className='w-full h-5' src={telegram} alt="соц. сеть telegram" title='соц. сеть telegram' aria-label='соц. сеть telegram'/></li>
+        <li className='cursor-pointer hover:opacity-60 w-5'><img className='w-full h-5' src={viber} alt="соц. сеть viber" title='соц. сеть viber' aria-label='соц. сеть viber'/></li>
+        <li className='cursor-pointer hover:opacity-60 w-5'><img className='w-full h-5' src={whatsapp} alt="соц. сеть whatsapp" title='соц. сеть whatsapp' aria-label='соц. сеть whatsapp'/></li>
     </>
   )
 }
